d959a0b671564de02854b5c2f5f63976fa57fd56
Hanut <hanutsing@gmail.com>
2017-12-13 17:24:19 +0530

    Fixed the + to be added infront of number being added at the end.

--- index.js
+++ index.js
@@ -1,47 +1,47 @@
 var cc = require('./cc.json');
 
 module.exports = {
-    split: function (phone, cb) {
-        try{
-	        var countryCode = "";
-	        if (phone[0] != "+") {
-				phone += "+";
-			}
-            countryCode = phone.substr(0, 5);
-	        var foundCC = null;
-	        cc.forEach((data)=>{
-				if(countryCode.indexOf(data.dial_code) !== -1){
-					foundCC = data.dial_code;
-				}
-	        });
-	        if(foundCC==null){
-	        	return cb("Not Found")
-	        }
-	        console.log("CC length %s \nPhone length : %s",foundCC.length,phone.length)
-			phone = phone.substr(foundCC.length,phone.length-foundCC.length);
-	        return cb(null, {cc:foundCC,phone:phone});
-        }catch(e){
-        	cb(err);
+  split: function (phone, cb) {
+    try{
+      var countryCode = "";
+      if (phone[0] != "+") {
+        phone = "+"+phone;
+      }
+      countryCode = phone.substr(0, 5);
+      var foundCC = null;
+      cc.forEach((data)=>{
+        if(countryCode.indexOf(data.dial_code) !== -1){
+          foundCC = data.dial_code;
         }
-    },
-    
-    cleanNumber: function(phone, cc, cb) {
-      if(phone.match(/^[+]+[0-9]/)){
-        phone = phone.replace(/^[+]+/, "+");
-        if(phone.length>6 && phone.length<18){
-          console.log("number : "+phone)
-          return cb(null, phone, true)
-        } else {
-          return cb("Invalid number")
-        }
+      });
+      if(foundCC==null){
+        return cb("Not Found")
+      }
+      console.log("CC length %s \nPhone length : %s",foundCC.length,phone.length)
+      phone = phone.substr(foundCC.length,phone.length-foundCC.length);
+      return cb(null, {cc:foundCC,phone:phone});
+    } catch (e) {
+      cb(err);
+    }
+  },
+
+  cleanNumber: function(phone, cc, cb) {
+    if(phone.match(/^[+]+[0-9]/)){
+      phone = phone.replace(/^[+]+/, "+");
+      if(phone.length>6 && phone.length<18){
+        console.log("number : "+phone)
+        return cb(null, phone, true)
       } else {
-        if(phone.length>6 && phone.length<18) {
-          phone = phone.replace(/[+]/, "");
-          phone = phone.replace(/^[0]*/, "");
-          return cb(null, phone, false);
-        } else {
-          return cb("Invalid number")
-        }
+        return cb("Invalid number")
+      }
+    } else {
+      if(phone.length>6 && phone.length<18) {
+        phone = phone.replace(/[+]/, "");
+        phone = phone.replace(/^[0]*/, "");
+        return cb(null, phone, false);
+      } else {
+        return cb("Invalid number")
       }
     }
+  }
 }
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 var phonecc = require('./index.js');
 
-phonecc.split("+918126723680",(err, result) => {
+phonecc.split("918126723680",(err, result) => {
     if (err) {
         console.log(err);
     } else {
